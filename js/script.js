b25b07837377effe34bc26eb14fd3f57f6e828b9
Karolina Piróg <karolina.swieca@wp.pl>
2018-04-19 11:23:10 +0200

    add simply html and function phone in js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function Phone(brand, price, color) {
+    this.brand = brand;
+    this.price = price;
+    this.color = color;
+}
+
+Phone.prototype.printInfo = function() {
+    console.log("The phone brand is: " + this.brand + " color is: " + this.color + " and the price is: " + this.price + ".");
+}
+
+var iPhone6s = new Phone("Apple", 2250, "silver");
+
+iPhone6s.printInfo();
+
+var SamsungGalaxyS6 = new Phone("Samsung", 1000, "gold");
+
+SamsungGalaxyS6.printInfo();
+
+var OnePlusOne = new Phone("OnePlus", 1200, "black");
+
+OnePlusOne.printInfo();
